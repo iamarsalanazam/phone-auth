e36cdd75d626dab032950c91b1555c04bc8509f3
Arsalan <iamarsalanazam@gmail.com>
2023-05-24 20:03:59 +0500

    Remove login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,60 +11,29 @@ const Login = () => {
   const { logIn } = useUserAuth();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await logIn(email, password);
-      navigate("/home");
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError("");
+  //   try {
+  //     await logIn(email, password);
+  //     navigate("/home");
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
   return (
     <>
-      <div className="p-4 box">
+      <div className="p-4 box container">
         <h2 className="mb-3">Firebase Auth Login</h2>
         {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <div className="d-flex">
-            <div className="mr-3">
-              <Button variant="primary" type="Submit">
-                Log In
-              </Button>
-            </div>
-            <div>
-              <Link to="/signinotp">
-                <Button variant="secondary" className="mx-3">
-                  Sign In with OTP
-                </Button>
-              </Link>
-            </div>
+        <Form>
+          <div>
+            <Link to="/signinotp">
+              <Button variant="secondary">Sign In with OTP</Button>
+            </Link>
           </div>
         </Form>
-        {/* <div>
-          <GoogleButton
-            className="g-btn"
-            type="dark"
-            onClick={handleGoogleSignIn}
-          />
-        </div> */}
       </div>
     </>
   );
--- src/components/SignInOtp.jsx
+++ src/components/SignInOtp.jsx
@@ -57,7 +57,7 @@ function SignInOtp() {
           <div className="d-flex mt-3">
             <div className="mr-3">
               <Link to="/">
-                <Button variant="primary" type="Submit" className="">
+                <Button variant="primary" type="Submit" className="mr-3">
                   Cancel
                 </Button>
               </Link>
