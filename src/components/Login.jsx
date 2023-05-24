5bd219a47489ac3f05fefd6d40ecfc74e9e0b172
Arsalan <iamarsalanazam@gmail.com>
2023-05-24 20:04:08 +0500

    login changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
   return (
     <>
-      <div className="p-4 box container">
+      <div className="p-4 box">
         <h2 className="mb-3">Firebase Auth Login</h2>
         {error && <Alert variant="danger">{error}</Alert>}
         <Form>
